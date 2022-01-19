76a583b9c9784a6bb0acc79dca6e80b342ad5139
mat <aadrian.mat@gmail.com>
2022-01-19 09:59:42 +0100

    black formatting

--- src/restapi/app/ray_wraper_test.py
+++ src/restapi/app/ray_wraper_test.py
@@ -13,9 +13,6 @@ class TestModelPipelineMlflow:
         with open(f"./user-examples/user_0.json", "rb") as file:
             user_dict = json.load(file)
 
-        response = requests.post(
-            url="http://127.0.0.1:8000/predict",
-            json=user_dict
-        )
+        response = requests.post(url="http://127.0.0.1:8000/predict", json=user_dict)
 
-        assert response.json() == {'Prediction': 1}
+        assert response.json() == {"Prediction": 1}
